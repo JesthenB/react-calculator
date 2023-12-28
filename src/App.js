a7a89b5aacdecf4f2b04959865031d79cc89fef8
JesthenB <jesthenh@gmail.com>
2023-12-28 16:05:20 -0600

    finished logic for evaluation and deleting so now you can delete the last number inputted into the calculator or delete the evaluation completely

--- src/App.js
+++ src/App.js
@@ -15,6 +15,13 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false,
+        };
+      }
       if (payload.digit === "0" && state.currentOperand === "0") return state;
       if (payload.digit === "." && state.currentOperand.includes("."))
         return state;
@@ -27,11 +34,11 @@ function reducer(state, { type, payload }) {
         return state;
       }
 
-      if (state.currentOperand == null){
+      if (state.currentOperand == null) {
         return {
-          ...state, 
+          ...state,
           operation: payload.operation,
-        }
+        };
       }
 
       if (state.previousOperand == null) {
@@ -52,6 +59,40 @@ function reducer(state, { type, payload }) {
 
     case ACTIONS.CLEAR:
       return {};
+
+      case ACTIONS.DELETE_DIGIT:
+        if(state.overwrite){ 
+          return {
+            ...state,
+            overwrite:false,
+            currentOperand: null,
+          }
+        }
+        if (state.currentOperand == null) return state
+        if (state.currentOperand.length === 1){
+          return {...state, currentOperand:null}
+        }
+
+        return {
+          ...state,
+          currentOperand: state.currentOperand.slice(0, -1)
+        }
+
+    case ACTIONS.EVALUATE:
+      if (
+        state.operation == null ||
+        state.currentOperand == null ||
+        state.previousOperand == null
+      ) {
+        return state;
+      }
+      return {
+        ...state,
+        overwrite: true,
+        previousOperand: null,
+        operation: null,
+        currentOperand: evaluate(state),
+      };
   }
 }
 
@@ -71,6 +112,10 @@ function evaluate({ currentOperand, previousOperand, operation }) {
 
     case "÷":
       computation = prev / current;
+      break;
+
+      case "x":
+      computation = prev * current;
       break;
   }
   return computation.toString();
@@ -96,7 +141,7 @@ function App() {
       >
         AC
       </button>
-      <button>DEL</button>
+      <button  onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>DEL</button>
       <OperationButton operation="÷" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
@@ -112,7 +157,12 @@ function App() {
       <OperationButton operation="-" dispatch={dispatch} />
       <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
-      <button className="span-two">=</button>
+      <button
+        className="span-two"
+        onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
+      >
+        =
+      </button>
     </div>
   );
 }
